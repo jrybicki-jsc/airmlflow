18e8d5f8d6c47629b7faca4acae58a665e1c0b1e
jrybicki-jsc <j.rybicki@fz-juelich.de>
2019-10-15 09:55:21 +0200

    works with data copying from openaq

--- airflow/dags/trba.py
+++ airflow/dags/trba.py
@@ -6,19 +6,31 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import Variable
 from sqlalchemy import create_engine
+from airflow.hooks.postgres_hook import PostgresHook
 
-def setup_dat(*args, **kwargs):
-    #tdir = Variable.get('target_dir')
+
+def setup_new(*args, **kwargs):
+    sql = '''select * from measurement where series_id=242;'''
     db_url = kwargs['db_url']
-    logging.info(f'Setting up data {db_url}')
     engine = create_engine(db_url, echo=True)
     conn = engine.connect()
     conn.execute('CREATE table IF NOT EXISTS data(x float, value float)')
+    conn.execute('DELETE FROM data')
 
-    for i in range(-2, 2, 1):
-        conn.execute(f'INSERT into data(x, value) values({i},{1 if i>0 else 0} )')
-    conn.close()
-    return True
+    try:
+        pg = PostgresHook(postgres_conn_id='openaq-db')
+        df = pg.get_pandas_df(sql, parameters=None)
+        print(f'got the df: {df}')
+        print(f'{df.columns}')
+        
+        for x, y in df['value'].iteritems():
+            conn.execute(f'INSERT into data(x, value) values({x},{y} )')
+
+        conn.close()            
+    except:
+        logging.error(
+            'Remote database not defined. Use [openaq-db] connection')
+        return None
 
 def test_data(*args, **kwargs):
     db_url = kwargs['db_url']
@@ -54,7 +66,7 @@ git_url = 'https://github.com/jrybicki-jsc/aflow.git'
 #git_url = 'https://github.com/mlflow/mlflow-example'
 
 with dag:
-    setup_data = PythonOperator(task_id='setup2', python_callable=setup_dat, op_kwargs=op_kwargs)
+    setup_data = PythonOperator(task_id='setup2', python_callable=setup_new, op_kwargs=op_kwargs)
     test_data = PythonOperator(task_id='test_dat', python_callable=test_data, op_kwargs=op_kwargs)
     train_model = BashOperator(task_id='train_model_bash',
                                bash_command=f'source /Users/jj/miniconda3/bin/../etc/profile.d/conda.sh && conda activate mlflow && mlflow run {git_url} -P alpha=0.4',
